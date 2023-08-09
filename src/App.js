59887a829823ad9bd19b494d56a1fdc1ec66606f
catupik <gnedyshevaeg@gmail.com>
2023-08-09 10:29:55 +0200

    barbie first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+  const [facts, setFacts] = useState(data);
+  const [showText, setShowText] = useState(false);
+
+  const removeFact = (id) => {
+    let newArray = facts.filter(fact => fact.id !== id);
+    setFacts(newArray);
+  }
+
+  const showTextClick = (item) => {
+    item.showMore = !item.showMore;
+    setShowText(!showText)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1 className='container'>{facts.length} Things You Need to Know About Game Developer Barbie</h1>
+      {facts.map((item=>{
+        const {id, fact, description, image, showMore} = item;
+        return(
+          <div  key={id}>
+            <h2 className='container'>{id} - {fact}</h2>
+
+            <div className='container'>
+            <img  src={image} width='300' alt='barbie'/>
+            </div>
+            <div className='container'>
+            <p >
+              {showMore ? description : description.substring(0, 250) + '...'}
+              <button onClick={()=> showTextClick(item)}>{showMore ? 'Show less' : 'Show more'}</button>
+            </p>
+            </div>
+
+            <div className='container'>
+              <button className='btn' onClick={()=> removeFact(id)}>Delete Fact</button>
+            </div>
+
+            <br/> <br/> <br/>
+
+          </div>
+        )
+      }))}
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        id: 1,
+        fact: "She actually looks like a game developer.",
+        description: "As Mattel puts it, Game Developer Barbie’s style is “industry-inspired,” and it’s true that, unlike the usual glam Barbie look, this doll wouldn’t be out of place at a start up’s office. While Computer Engineer Barbie rocked a ton of eyeshadow and sparkly leggings, Developer Barbie looks casual yet cute with faded jeans, a t-shirt, and an army-green jacket. Instead of the standard painful-looking Barbie pumps, she’s sporting chic white sneakers, and a pair of trendy thick-frame glasses rounds out the look. Like many real-life developers, Barbie looks hip, but in a laid-back, kinda nerdy way.",
+        image: "https://www.format.com/wp-content/uploads/computer-engineer-barbie-3.jpg",
+        showMore: false,
+    },
+
+    {
+        id: 2,
+        fact: "She has regular tech gear instead of all pink everything.",
+        description: "Since Computer Engineer Barbie was working with an improbably matching, neon pink smartphone and laptop set, it’s refreshing to see this Barbie with a regular laptop, tablet, and headset. No more cutesy pink hardware. Developer Barbie’s tiny plastic devices are just like real ones an actual game developer might use at work—right down to the goofy robot sticker on her laptop.",
+        image: "https://www.format.com/wp-content/uploads/game-developer-barbie-mattel-3.jpg",
+        showMore: false,
+    },
+
+    {
+        id: 3,
+        fact: "She has a realistic workspace.",
+        description: "Retro Barbie’s work desk was cluttered with magazines and photos of boyfriend Ken. And it was hot pink, of course. But Game Developer Barbie comes in a package depicting her desk exactly as you’d want to imagine it: huge mug of coffee for staying focused on deadlines, take-out boxes (Barbie celebrates finishing a project in style), trash bin full of old note paper from productive brainstorming sessions, sensible ergonomic chair, Beats-style headphones for unwinding with her favorite tunes. There are still a few touches of pink in there, because even badass developers can be girly if they want to.",
+        image: "https://www.format.com/wp-content/uploads/barbie-computer-1990s.jpg",
+        showMore: false,
+    },
+
+    {
+        id: 4,
+        fact: "She knows how to code.",
+        description: "While Computer Engineer Barbie was called out for coding in binary, Game Developer Barbie kicks things up a notch by actually knowing real code. Tech researcher Casey Fiesler has identified the interface on Barbie’s tiny screen as similar to the educational programming environment Alice, and says that “she seems to be making a Bejeweled clone in Flash.” Developer Barbie, unlike Computer Engineer Barbie, is depicted doing something beginner developers might actually do.",
+        image: "https://www.format.com/wp-content/uploads/game-developer-barbie-mattel-1.jpg",
+        showMore: false,
+    },
+
+    {
+        id: 5,
+        fact: "She knows there’s more to game development than coding.",
+        description: "I Can Be A Computer Engineer, the picture book which accompanied Computer Engineer Barbie, was widely panned across the Internet for failing to provide a realistic depiction of what being a computer engineer might entail. Wasting a great opportunity to show young girls different ways they might be able to get involved in game development, the book instead showed Barbie giving her sister’s laptop a virus and then getting her male classmates to fix it for her. Despite supposedly being a computer engineer, Barbie was just creating “design ideas” for her planned video game, and needed the boys to do the coding. Mattel eventually pulled the book and promised more empowering stories from future Barbies.",
+        image: "https://helios-i.mashable.com/imagery/articles/074ZJKhHrIqw39n4DAIQDHu/hero-image.fill.size_1200x900.v1611610064.png",
+        showMore: false,
+    }
+
+]
